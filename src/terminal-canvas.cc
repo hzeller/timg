d9d70dd414c2c77a562f49b00016a31af5c34ee3
Henner Zeller <h.zeller@acm.org>
2020-05-04 00:58:27 -0700

    Reduce TerminalCanvas CPU usage.
    The less time we use, the more the terminal has CPU time to
    decode stuff :)
    
    (Speedup in some artificial benchmark about 2.5-3x
     Now on my random i7 laptop can push about 3000fps for
     a 400x200 canvas, which means 2% CPU for 60fps).

--- src/terminal-canvas.cc
+++ src/terminal-canvas.cc
@@ -48,7 +48,9 @@ Framebuffer::rgb_t Framebuffer::at(int x, int y) const {
 }
 
 #define SCREEN_CLEAR    "\033c"
-#define SCREEN_RESET    "\033[0m"           // reset terminal settings
+#define SCREEN_END_OF_LINE  "\033[0m\n"      // reset terminal and newline.
+#define SCREEN_END_OF_LINE_LEN strlen(SCREEN_END_OF_LINE)
+
 #define SCREEN_CURSOR_UP_FORMAT "\033[%dA"  // Move cursor up given lines.
 
 // Interestingly, cursor-on does not take effect until the next newline on
@@ -69,8 +71,11 @@ Framebuffer::rgb_t Framebuffer::at(int x, int y) const {
 // bottom block by default.
 #define PIXEL_UPPER_HALF_BLOCK_CHARACTER  "▀"
 #define PIXEL_LOWER_HALF_BLOCK_CHARACTER  "▄"
+#define PIXEL_BLOCK_CHARACTER_LENGTH strlen(PIXEL_UPPER_HALF_BLOCK_CHARACTER)
+
 #define PIXEL_SET_FOREGROUND_COLOR        "38;2;"
 #define PIXEL_SET_BACKGROUND_COLOR        "48;2;"
+#define PIXEL_SET_COLOR_LENGTH       strlen(PIXEL_SET_FOREGROUND_COLOR)
 
 static void reliable_write(int fd, const char *buf, size_t size) {
     int written;
@@ -92,11 +97,10 @@ char *TerminalCanvas::EnsureBuffer(int width, int height, int indent) {
 
     const int vertical_characters = (height+1) / 2;   // two pixels, one glyph
     const size_t character_buffer_size = vertical_characters *
-        (indent                    // Horizontal indentation with spaces
-         + width * max_pixel_size  // pixels in one row
-         + strlen(SCREEN_RESET)    // Reset at each EOL
-         + 1)                      // one \n per line
-        + 1;                       // one potential NUL
+        (indent                     // Horizontal indentation with spaces
+         + width * max_pixel_size   // pixels in one row
+         + SCREEN_END_OF_LINE_LEN)  // Reset at each EOL
+        + 1;                        // one potential NUL
 
     if (character_buffer_size > buffer_size_) {
         if (!content_buffer_) {
@@ -128,18 +132,7 @@ TerminalCanvas::~TerminalCanvas() {
     free(content_buffer_);
 }
 
-static inline char *int_append(char *buf, uint8_t val) {
-    // There are probably faster ways.
-    if (val >= 100) { *buf++ = (val / 100) + '0'; }
-    if (val >= 10)  { *buf++ = ((val / 10) % 10) + '0'; }
-    *buf++ = (val % 10) + '0';
-    return buf;
-}
-static inline char *str_append(char *buf, const char *str) {
-    while ((*buf++ = *str++))
-        ;
-    return buf - 1;
-}
+static char *int_append(char *buf, uint8_t val);
 static char *WriteAnsiColor(char *buf, uint32_t col) {
     buf = int_append(buf, (col & 0xff0000) >> 16);
     *buf++ = ';';
@@ -163,32 +156,46 @@ static char *AppendDoubleRow(
         pos += indent;
     }
     for (int x = 0; x < width; ++x) {
-        const char *prefix = kStartEscape;
+        bool color_emitted = false;
         if (top_line) {
             const Framebuffer::rgb_t top = *top_line++;
             if (top != last_top) {
-                pos = str_append(pos, prefix);
-                pos = str_append(pos, set_top_pixel_color);
+                // Appending prefix. At this point, it can only be kStartEscape
+                memcpy(pos, kStartEscape, strlen(kStartEscape));
+                pos += strlen(kStartEscape);
+                memcpy(pos, set_top_pixel_color, PIXEL_SET_COLOR_LENGTH);
+                pos += PIXEL_SET_COLOR_LENGTH;
                 pos = WriteAnsiColor(pos, top);
                 last_top = top;
-                prefix = ";";
+                color_emitted = true;
             }
         }
         if (bottom_line) {
             const Framebuffer::rgb_t btm = *bottom_line++;
             if (btm != last_btm) {
-                pos = str_append(pos, prefix);
-                pos = str_append(pos, set_bottom_pixel_color);
+                if (color_emitted) {
+                    *pos++ = ';';
+                } else {
+                    memcpy(pos, kStartEscape, strlen(kStartEscape));
+                    pos += strlen(kStartEscape);
+                }
+                memcpy(pos, set_bottom_pixel_color, PIXEL_SET_COLOR_LENGTH);
+                pos += PIXEL_SET_COLOR_LENGTH;
                 pos = WriteAnsiColor(pos, btm);
                 last_btm = btm;
-                prefix = nullptr;   // Sentinel for next if()
+                color_emitted = true;
             }
         }
-        if (prefix != kStartEscape) {
+        if (color_emitted) {
             *pos++ = 'm';   // We emitted color; need to finish ESC seq
         }
-        pos = str_append(pos, pixel_glyph);
+        memcpy(pos, pixel_glyph, PIXEL_BLOCK_CHARACTER_LENGTH);
+        pos += PIXEL_BLOCK_CHARACTER_LENGTH;
     }
+
+    memcpy(pos, SCREEN_END_OF_LINE, SCREEN_END_OF_LINE_LEN);
+    pos += SCREEN_END_OF_LINE_LEN;
+
     return pos;
 }
 
@@ -214,8 +221,6 @@ void TerminalCanvas::Send(const Framebuffer &framebuffer, int indent) {
                               top_line, set_upper_color_,
                               bottom_line, set_lower_color_,
                               pixel_character_);
-        pos = str_append(pos, SCREEN_RESET);  // end-of-line
-        *pos++ = '\n';
     }
     reliable_write(fd_, start_buffer, pos - start_buffer);
 }
@@ -235,5 +240,75 @@ void TerminalCanvas::CursorOff() {
 
 void TerminalCanvas::CursorOn() {
     reliable_write(fd_, CURSOR_ON, strlen(CURSOR_ON));
+}
+
+// Somewhat fast conversion uint8 -> ASCII digits. There are probably faster
+// ways, but this is a good start.
+// Approach is to know exactly how many digits to memcpy(), which helps the
+// compiler create good instructions.
+
+// Alignment of 4 seems to make gcc most happy.
+static const char digit_convert[256][4] = {
+  // Single digits not used, just for appropriate space-holding
+  {'0',       },{'1',       },{'2',       },{'3',       },{'4',       },
+  {'5',       },{'6',       },{'7',       },{'8',       },{'9',       },
+
+  {'1','0',   },{'1','1',   },{'1','2',   },{'1','3',   },{'1','4',   },
+  {'1','5',   },{'1','6',   },{'1','7',   },{'1','8',   },{'1','9',   },
+  {'2','0',   },{'2','1',   },{'2','2',   },{'2','3',   },{'2','4',   },
+  {'2','5',   },{'2','6',   },{'2','7',   },{'2','8',   },{'2','9',   },
+  {'3','0',   },{'3','1',   },{'3','2',   },{'3','3',   },{'3','4',   },
+  {'3','5',   },{'3','6',   },{'3','7',   },{'3','8',   },{'3','9',   },
+  {'4','0',   },{'4','1',   },{'4','2',   },{'4','3',   },{'4','4',   },
+  {'4','5',   },{'4','6',   },{'4','7',   },{'4','8',   },{'4','9',   },
+  {'5','0',   },{'5','1',   },{'5','2',   },{'5','3',   },{'5','4',   },
+  {'5','5',   },{'5','6',   },{'5','7',   },{'5','8',   },{'5','9',   },
+  {'6','0',   },{'6','1',   },{'6','2',   },{'6','3',   },{'6','4',   },
+  {'6','5',   },{'6','6',   },{'6','7',   },{'6','8',   },{'6','9',   },
+  {'7','0',   },{'7','1',   },{'7','2',   },{'7','3',   },{'7','4',   },
+  {'7','5',   },{'7','6',   },{'7','7',   },{'7','8',   },{'7','9',   },
+  {'8','0',   },{'8','1',   },{'8','2',   },{'8','3',   },{'8','4',   },
+  {'8','5',   },{'8','6',   },{'8','7',   },{'8','8',   },{'8','9',   },
+  {'9','0',   },{'9','1',   },{'9','2',   },{'9','3',   },{'9','4',   },
+  {'9','5',   },{'9','6',   },{'9','7',   },{'9','8',   },{'9','9',   },
+  {'1','0','0'},{'1','0','1'},{'1','0','2'},{'1','0','3'},{'1','0','4'},
+  {'1','0','5'},{'1','0','6'},{'1','0','7'},{'1','0','8'},{'1','0','9'},
+  {'1','1','0'},{'1','1','1'},{'1','1','2'},{'1','1','3'},{'1','1','4'},
+  {'1','1','5'},{'1','1','6'},{'1','1','7'},{'1','1','8'},{'1','1','9'},
+  {'1','2','0'},{'1','2','1'},{'1','2','2'},{'1','2','3'},{'1','2','4'},
+  {'1','2','5'},{'1','2','6'},{'1','2','7'},{'1','2','8'},{'1','2','9'},
+  {'1','3','0'},{'1','3','1'},{'1','3','2'},{'1','3','3'},{'1','3','4'},
+  {'1','3','5'},{'1','3','6'},{'1','3','7'},{'1','3','8'},{'1','3','9'},
+  {'1','4','0'},{'1','4','1'},{'1','4','2'},{'1','4','3'},{'1','4','4'},
+  {'1','4','5'},{'1','4','6'},{'1','4','7'},{'1','4','8'},{'1','4','9'},
+  {'1','5','0'},{'1','5','1'},{'1','5','2'},{'1','5','3'},{'1','5','4'},
+  {'1','5','5'},{'1','5','6'},{'1','5','7'},{'1','5','8'},{'1','5','9'},
+  {'1','6','0'},{'1','6','1'},{'1','6','2'},{'1','6','3'},{'1','6','4'},
+  {'1','6','5'},{'1','6','6'},{'1','6','7'},{'1','6','8'},{'1','6','9'},
+  {'1','7','0'},{'1','7','1'},{'1','7','2'},{'1','7','3'},{'1','7','4'},
+  {'1','7','5'},{'1','7','6'},{'1','7','7'},{'1','7','8'},{'1','7','9'},
+  {'1','8','0'},{'1','8','1'},{'1','8','2'},{'1','8','3'},{'1','8','4'},
+  {'1','8','5'},{'1','8','6'},{'1','8','7'},{'1','8','8'},{'1','8','9'},
+  {'1','9','0'},{'1','9','1'},{'1','9','2'},{'1','9','3'},{'1','9','4'},
+  {'1','9','5'},{'1','9','6'},{'1','9','7'},{'1','9','8'},{'1','9','9'},
+  {'2','0','0'},{'2','0','1'},{'2','0','2'},{'2','0','3'},{'2','0','4'},
+  {'2','0','5'},{'2','0','6'},{'2','0','7'},{'2','0','8'},{'2','0','9'},
+  {'2','1','0'},{'2','1','1'},{'2','1','2'},{'2','1','3'},{'2','1','4'},
+  {'2','1','5'},{'2','1','6'},{'2','1','7'},{'2','1','8'},{'2','1','9'},
+  {'2','2','0'},{'2','2','1'},{'2','2','2'},{'2','2','3'},{'2','2','4'},
+  {'2','2','5'},{'2','2','6'},{'2','2','7'},{'2','2','8'},{'2','2','9'},
+  {'2','3','0'},{'2','3','1'},{'2','3','2'},{'2','3','3'},{'2','3','4'},
+  {'2','3','5'},{'2','3','6'},{'2','3','7'},{'2','3','8'},{'2','3','9'},
+  {'2','4','0'},{'2','4','1'},{'2','4','2'},{'2','4','3'},{'2','4','4'},
+  {'2','4','5'},{'2','4','6'},{'2','4','7'},{'2','4','8'},{'2','4','9'},
+  {'2','5','0'},{'2','5','1'},{'2','5','2'},{'2','5','3'},{'2','5','4'},
+  {'2','5','5'}
+};
+
+static char *int_append(char *buf, uint8_t val) {
+    if (val >= 100) { memcpy(buf, digit_convert[val], 3); return buf + 3; }
+    if (val >= 10) { memcpy(buf, digit_convert[val], 2); return buf + 2; }
+    *buf++ = val + '0';
+    return buf;
 }
 }  // namespace timg
