a5c50db1f6c6755d5bcc7416dbf7a88310b71bba
Henner Zeller <h.zeller@acm.org>
2020-05-04 19:55:03 -0700

    Include semicolon in integer printing - always needed anyway.

--- src/terminal-canvas.cc
+++ src/terminal-canvas.cc
@@ -130,13 +130,11 @@ TerminalCanvas::~TerminalCanvas() {
     free(content_buffer_);
 }
 
-static char *int_append(char *buf, uint8_t val);
+static char *int_append_with_semicolon(char *buf, uint8_t val);
 static char *WriteAnsiColor(char *buf, uint32_t col) {
-    buf = int_append(buf, (col & 0xff0000) >> 16);
-    *buf++ = ';';
-    buf = int_append(buf, (col & 0xff00) >> 8);
-    *buf++ = ';';
-    return int_append(buf, (col & 0xff));
+    buf = int_append_with_semicolon(buf, (col & 0xff0000) >> 16);
+    buf = int_append_with_semicolon(buf, (col & 0xff00) >> 8);
+    return int_append_with_semicolon(buf, (col & 0xff));
 }
 
 static inline char *str_append(char *pos, const char *value, size_t len) {
@@ -174,9 +172,7 @@ static char *AppendDoubleRow(
         if (bottom_line) {
             const Framebuffer::rgb_t bottom_color = *bottom_line++;
             if (bottom_color != last_bottom_color) {
-                if (color_emitted) {
-                    *pos++ = ';';
-                } else {
+                if (!color_emitted) {
                     pos = str_append(pos, kStartEscape, strlen(kStartEscape));
                 }
                 pos = str_append(pos, set_btm_pixel_color, PIXEL_SET_COLOR_LEN);
@@ -186,7 +182,7 @@ static char *AppendDoubleRow(
             }
         }
         if (color_emitted) {
-            *pos++ = 'm';   // We emitted color; need to finish ESC seq
+            *(pos-1) = 'm';   // overwrite semicolon with finish ESC seq.
         }
         pos = str_append(pos, pixel_glyph, PIXEL_BLOCK_CHARACTER_LEN);
     }
@@ -254,57 +250,50 @@ void TerminalCanvas::CursorOn() {
 // TODO: is there a way to force-align a string literal to 4 ? We could write
 // these as an uint32 array, but it would look less readable.
 static const char digit_convert[] =
-    // Note: Single digits not used, just for appropriate array space-holding.
-    "0   1   2   3   4   5   6   7   8   9   "
-
-    // Two digit numbers. They are left aligned, as we copy the first 2 bytes.
-    "10  11  12  13  14  15  16  17  18  19  "
-    "20  21  22  23  24  25  26  27  28  29  "
-    "30  31  32  33  34  35  36  37  38  39  "
-    "40  41  42  43  44  45  46  47  48  49  "
-    "50  51  52  53  54  55  56  57  58  59  "
-    "60  61  62  63  64  65  66  67  68  69  "
-    "70  71  72  73  74  75  76  77  78  79  "
-    "80  81  82  83  84  85  86  87  88  89  "
-    "90  91  92  93  94  95  96  97  98  99  "
-
-    // All three digit numbers up to 255, copying out 3.
-    // In fact, we copy 4 bytes, as 32-bit handling is cheaper natively.
-    // Despite memory look-up overhead, this measures still faster than adding
-    // more branches or calculations to int_append() (at lesat on x86).
-    "100 101 102 103 104 105 106 107 108 109 "
-    "110 111 112 113 114 115 116 117 118 119 "
-    "120 121 122 123 124 125 126 127 128 129 "
-    "130 131 132 133 134 135 136 137 138 139 "
-    "140 141 142 143 144 145 146 147 148 149 "
-    "150 151 152 153 154 155 156 157 158 159 "
-    "160 161 162 163 164 165 166 167 168 169 "
-    "170 171 172 173 174 175 176 177 178 179 "
-    "180 181 182 183 184 185 186 187 188 189 "
-    "190 191 192 193 194 195 196 197 198 199 "
-    "200 201 202 203 204 205 206 207 208 209 "
-    "210 211 212 213 214 215 216 217 218 219 "
-    "220 221 222 223 224 225 226 227 228 229 "
-    "230 231 232 233 234 235 236 237 238 239 "
-    "240 241 242 243 244 245 246 247 248 249 "
-    "250 251 252 253 254 255 ";
+    "0;  1;  2;  3;  4;  5;  6;  7;  8;  9;  "
+    "10; 11; 12; 13; 14; 15; 16; 17; 18; 19; "
+    "20; 21; 22; 23; 24; 25; 26; 27; 28; 29; "
+    "30; 31; 32; 33; 34; 35; 36; 37; 38; 39; "
+    "40; 41; 42; 43; 44; 45; 46; 47; 48; 49; "
+    "50; 51; 52; 53; 54; 55; 56; 57; 58; 59; "
+    "60; 61; 62; 63; 64; 65; 66; 67; 68; 69; "
+    "70; 71; 72; 73; 74; 75; 76; 77; 78; 79; "
+    "80; 81; 82; 83; 84; 85; 86; 87; 88; 89; "
+    "90; 91; 92; 93; 94; 95; 96; 97; 98; 99; "
+    "100;101;102;103;104;105;106;107;108;109;"
+    "110;111;112;113;114;115;116;117;118;119;"
+    "120;121;122;123;124;125;126;127;128;129;"
+    "130;131;132;133;134;135;136;137;138;139;"
+    "140;141;142;143;144;145;146;147;148;149;"
+    "150;151;152;153;154;155;156;157;158;159;"
+    "160;161;162;163;164;165;166;167;168;169;"
+    "170;171;172;173;174;175;176;177;178;179;"
+    "180;181;182;183;184;185;186;187;188;189;"
+    "190;191;192;193;194;195;196;197;198;199;"
+    "200;201;202;203;204;205;206;207;208;209;"
+    "210;211;212;213;214;215;216;217;218;219;"
+    "220;221;222;223;224;225;226;227;228;229;"
+    "230;231;232;233;234;235;236;237;238;239;"
+    "240;241;242;243;244;245;246;247;248;249;"
+    "250;251;252;253;254;255;";
 
 // Append decimal representation of given "value" to "buffer".
 // Does not \0-terminate. Might write one byte beyond number.
-static char *int_append(char *buffer, uint8_t value) {
+static char *int_append_with_semicolon(char *buffer, uint8_t value) {
     // We cheat a little here: for the beauty of initizliaing the above array
     // with a block of text, we hope for a nice 4 alignment of the bytes and
     // treat it as an uint32 array.
     const uint32_t *const four_bytes = (const uint32_t*) digit_convert;
     if (value >= 100) {
-        memcpy(buffer, &four_bytes[value], 4);  // copy 4 cheaper than 3
-        return buffer + 3;
+        memcpy(buffer, &four_bytes[value], 4);
+        return buffer + 4;
     }
     if (value >= 10) {
-        memcpy(buffer, &four_bytes[value], 4);
-        return buffer + 2;
+        memcpy(buffer, &four_bytes[value], 4); // copy 4 cheaper than 3
+        return buffer + 3;
     }
-    *buffer = value + '0';
-    return buffer + 1;
+    memcpy(buffer, &four_bytes[value], 2);
+    return buffer + 2;
 }
+
 }  // namespace timg
