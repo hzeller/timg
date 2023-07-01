765e54070deae1efe2acaaa6202118d8229fff18
Henner Zeller <h.zeller@acm.org>
2023-07-01 13:36:39 -0700

    Kitty output: create IDs by default.
    This is a compromise: makes WezTerm work with the kitty graphics
    protocol: if no ID is given, it assumes ID=0 and overwrites
    previous images if multiple images are given.
    
    Working around https://github.com/wez/wezterm/issues/3918

--- src/kitty-canvas.cc
+++ src/kitty-canvas.cc
@@ -16,6 +16,7 @@
 #include "kitty-canvas.h"
 
 #include <algorithm>
+#include <cstdint>
 
 #include "timg-base64.h"
 #include "timg-png.h"
@@ -27,6 +28,15 @@ static constexpr int kBase64EncodedChunkSize = 4096;  // Max allowed: 4096.
 static constexpr int kByteChunk              = kBase64EncodedChunkSize / 4 * 3;
 
 namespace timg {
+
+// Create ID unique enough for our purposes.
+static uint32_t CreateId() {
+    static const uint32_t kStart = (uint32_t)time(nullptr) << 7;
+    static uint32_t counter      = 0;
+    counter++;
+    return kStart + counter;
+}
+
 KittyGraphicsCanvas::KittyGraphicsCanvas(BufferedWriteSequencer *ws,
                                          ThreadPool *thread_pool,
                                          const DisplayOptions &opts)
@@ -56,20 +66,57 @@ void KittyGraphicsCanvas::Send(int x, int dy, const Framebuffer &fb_orig,
     char *const buffer    = RequestBuffer(fb->width(), fb->height());
     char *const offset    = AppendPrefixToBuffer(buffer);
 
-    const auto &options                   = options_;
-    std::function<OutBuffer()> encode_fun = [options, fb, buffer, offset]() {
+    const auto &opts = options_;
+
+    // Creating a new ID. Some terminals store the images in a GPU texture
+    // buffer and index by the ID, so we need to be economical with IDs.
+    uint32_t id = 0;
+    static uint32_t animation_id = 0;
+    static uint8_t flip_buffer = 0;
+    switch (seq_type) {
+    case SeqType::FrameImmediate:
+        // Ideally we use the content hash here. However, that means that
+        // if we have the same image in the same timg session, this would
+        // replace images. This could be addressed by remembering the ID
+        // and do a placement with the ID, however this is only really
+        // supported by Kitty directly while other compatible terminals can't
+        // deal with it yet reliably.
+        // So compromise: create unique ID for regular images.
+        id = CreateId();
+        break;
+    case SeqType::StartOfAnimation:
+        // Sending a bunch of images with different IDs overwhelms some
+        // terminals. So, for animations, just use two IDs back/forth.
+        id = CreateId();
+        animation_id = id;
+        break;
+    case SeqType::AnimationFrame:
+        ++flip_buffer;
+        id = animation_id + (flip_buffer % 2);
+        break;
+    case SeqType::ControlWrite:
+        ;
+        // should not happen.
+    }
+
+    std::function<OutBuffer()> encode_fun = [opts, fb, id, buffer, offset]() {
         std::unique_ptr<const Framebuffer> auto_delete(fb);
         const size_t png_buf_size = png::UpperBound(fb->width(), fb->height());
         std::unique_ptr<char[]> png_buf(new char[png_buf_size]);
 
-        int png_size = png::Encode(*fb, options.compress_pixel_level,
-                                   options.local_alpha_handling
+        int png_size = png::Encode(*fb, opts.compress_pixel_level,
+                                   opts.local_alpha_handling
                                        ? png::ColorEncoding::kRGB_24
                                        : png::ColorEncoding::kRGBA_32,
                                    png_buf.get(), png_buf_size);
 
         char *pos = offset;
-        pos += sprintf(pos, "\e_Ga=T,f=100,m=%d;", png_size > kByteChunk);
+        // Need to send an image with an id (i=...) as some terminals interpet
+        // no id with id=0 and just keep replacing one picture.
+        // Also need q=2 to prevent getting terminal feedback back we don't
+        // read.
+        pos += sprintf(pos, "\e_Ga=T,i=%u,q=2,f=100,m=%d;", id,
+                       png_size > kByteChunk);
         const char *png_data = png_buf.get();
         while (png_size) {
             int chunk_bytes = std::min(png_size, kByteChunk);
