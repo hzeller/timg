3a282b74d2c5dfac95703c37406950e1b5dc6d75
Mike Lei <mikelei8291@users.noreply.github.com>
2025-05-16 18:43:47 +0800

    Remove the call to the deprecated method `avcodec_close`
    Deprecated in ffmpeg 7.0: https://ffmpeg.org/doxygen/7.0/deprecated.html#_deprecated000003

--- src/video-source.cc
+++ src/video-source.cc
@@ -111,7 +111,6 @@ VideoSource::VideoSource(const std::string &filename) : ImageSource(filename) {
 
 VideoSource::~VideoSource() {
     sws_freeContext(sws_context_);
-    avcodec_close(codec_context_);
     avcodec_free_context(&codec_context_);
     avformat_close_input(&format_context_);
     delete terminal_fb_;
